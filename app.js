fde6c78cddaa4b369021fe38d3ba831d9321ae5e
KindaExists <ehrencastillo@gmail.com>
2021-08-17 15:02:32 +0800

    added function to create cards

--- app.js
+++ app.js
@@ -14,11 +14,44 @@ function addBookToLibrary(book) {
 
 function displayLibrary() {
     for (book of myLibrary) {
-        addTableRow(book);
+        createBookCard(book);
     }
 }
 
-const bookForm = document.forms[0];
+const cardContainer = document.querySelector('#card_container');
+
+function createBookCard(book) {
+    const newCard = document.createElement('div');
+    newCard.classList.add('card');
+
+    const title = document.createElement('h2');
+    const author = document.createElement('p');
+    const pages = document.createElement('p');
+    const readBtn = document.createElement('input');
+    const deleteBtn = document.createElement('button');
+
+
+    title.textContent = book.title;
+    author.textContent = `by ${book.author}`;
+    pages.textContent = `${book.pages} pages`;
+
+    readBtn.type = 'checkbox';
+    readBtn.checked = book.read;
+
+    deleteBtn.textContent = 'Delete';
+
+
+    newCard.appendChild(title);
+    newCard.appendChild(author);
+    newCard.appendChild(pages);
+    newCard.appendChild(readBtn);
+    newCard.appendChild(deleteBtn);
+
+    cardContainer.appendChild(newCard);
+}
+
+
+
 
 const book1 = new Book('book1', 'author1', 212, false);
 const book2 = new Book('book2', 'author2', 107, false);
