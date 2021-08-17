f9e4863da4dcdf68245cfac0d6c6add7346d67bc
KindaExists <ehrencastillo@gmail.com>
2021-08-17 14:16:03 +0800

    reverted code back to change model

--- app.js
+++ app.js
@@ -6,13 +6,7 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = () => {
-        const startStr = `${title} by ${author}, ${pages} pages, `;
-        const condStr = (read) ? 'Already read' : 'Not read yet';
-        return startStr + condStr;
-    };
 }
-
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
@@ -24,6 +18,7 @@ function displayLibrary() {
     }
 }
 
+const bookForm = document.forms[0];
 
 const book1 = new Book('book1', 'author1', 212, false);
 const book2 = new Book('book2', 'author2', 107, false);
@@ -32,22 +27,3 @@ const book3 = new Book('book3', 'author3', 54, true);
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
-
-const tbody = document.querySelector('#book_table').querySelector('tbody');
-
-function addTableRow(book) {
-    const newBookRow = tbody.insertRow();
-
-    const bookProperties = Object.getOwnPropertyNames(book).filter(prop => (typeof(book[prop]) !== 'function'));
-
-    bookProperties.forEach((prop) => {
-        const newCell = newBookRow.insertCell();
-        newCell.textContent = book[prop];
-        newBookRow.appendChild(newCell);
-    });
-
-    const deleteRowCell = newBookRow.insertCell();
-    deleteRowCell.textContent = 'Delete?'
-    newBookRow.appendChild(deleteRowCell);
-}
-
